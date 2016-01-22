2608e7484b5ffda43d0d10e5d18417cfa9186912
Marc-Antoine Lortie <marc-antoine.lortie@outlook.com>
2016-01-21 20:38:52 -0500

    Commit "Escape from the darkness" source files.
    Fixed:
    
    - "Could not load library client." when launching the mod.
    -  liblist.gam now has correct parameters.

---
+++ eftd/dlls/mp5.cpp
@@ -0,0 +1,494 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "monsters.h"
+#include "weapons.h"
+#include "nodes.h"
+#include "player.h"
+#include "soundent.h"
+#include "gamerules.h"
+
+enum mp5_e
+{
+	MP5_LONGIDLE = 0,
+	MP5_IDLE1,
+	MP5_LAUNCH,
+	MP5_RELOAD,
+	MP5_DEPLOY,
+	MP5_FIRE1,
+	MP5_FIRE2,
+	MP5_FIRE3,
+};
+
+
+
+LINK_ENTITY_TO_CLASS( weapon_mp5, CMP5 );
+LINK_ENTITY_TO_CLASS( weapon_9mmAR, CMP5 );
+
+
+//=========================================================
+//=========================================================
+int CMP5::SecondaryAmmoIndex( void )
+{
+	return m_iSecondaryAmmoType;
+}
+
+void CMP5::Spawn( )
+{
+	pev->classname = MAKE_STRING("weapon_9mmAR"); // hack to allow for old names
+	Precache( );
+	SET_MODEL(ENT(pev), "models/w_9mmAR.mdl");
+	m_iId = WEAPON_MP5;
+
+	m_iDefaultAmmo = MP5_DEFAULT_GIVE;
+
+#if defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+	m_fSpotActive = 1;
+#ifndef CLIENT_DLL
+	m_pSpot = NULL;
+#endif
+#endif // defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+
+	FallInit();// get ready to fall down.
+}
+
+
+void CMP5::Precache( void )
+{
+	PRECACHE_MODEL("models/v_9mmAR.mdl");
+	PRECACHE_MODEL("models/w_9mmAR.mdl");
+	PRECACHE_MODEL("models/p_9mmAR.mdl");
+
+	m_iShell = PRECACHE_MODEL ("models/shell.mdl");// brass shellTE_MODEL
+
+	PRECACHE_MODEL("models/grenade.mdl");	// grenade
+
+	PRECACHE_MODEL("models/w_9mmARclip.mdl");
+	PRECACHE_SOUND("items/9mmclip1.wav");              
+
+	PRECACHE_SOUND("items/clipinsert1.wav");
+	PRECACHE_SOUND("items/cliprelease1.wav");
+
+	PRECACHE_SOUND ("weapons/hks1.wav");// H to the K
+	PRECACHE_SOUND ("weapons/hks2.wav");// H to the K
+	PRECACHE_SOUND ("weapons/hks3.wav");// H to the K
+
+	PRECACHE_SOUND( "weapons/glauncher.wav" );
+	PRECACHE_SOUND( "weapons/glauncher2.wav" );
+
+	PRECACHE_SOUND ("weapons/357_cock1.wav");
+
+	m_usMP5 = PRECACHE_EVENT( 1, "events/mp5.sc" );
+	m_usMP52 = PRECACHE_EVENT( 1, "events/mp52.sc" );
+
+#if defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+	UTIL_PrecacheOther( "laser_spot" );
+#endif // defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+}
+
+int CMP5::GetItemInfo(ItemInfo *p)
+{
+	p->pszName = STRING(pev->classname);
+	p->pszAmmo1 = "9mm";
+	p->iMaxAmmo1 = _9MM_MAX_CARRY;
+	p->pszAmmo2 = "ARgrenades";
+	p->iMaxAmmo2 = M203_GRENADE_MAX_CARRY;
+	p->iMaxClip = MP5_MAX_CLIP;
+	p->iSlot = 2;
+	p->iPosition = 0;
+	p->iFlags = 0;
+	p->iId = m_iId = WEAPON_MP5;
+	p->iWeight = MP5_WEIGHT;
+
+	return 1;
+}
+
+int CMP5::AddToPlayer( CBasePlayer *pPlayer )
+{
+	if ( CBasePlayerWeapon::AddToPlayer( pPlayer ) )
+	{
+		MESSAGE_BEGIN( MSG_ONE, gmsgWeapPickup, NULL, pPlayer->pev );
+			WRITE_BYTE( m_iId );
+		MESSAGE_END();
+		return TRUE;
+	}
+	return FALSE;
+}
+
+BOOL CMP5::Deploy( )
+{
+#if defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+	BOOL bResult = DefaultDeploy( "models/v_9mmAR.mdl", "models/p_9mmAR.mdl", MP5_DEPLOY, "mp5" );
+
+	if (bResult)
+	{
+		m_pPlayer->m_flNextAttack = UTIL_WeaponTimeBase() + 1;
+	}
+
+	return bResult;
+#else
+	return DefaultDeploy( "models/v_9mmAR.mdl", "models/p_9mmAR.mdl", MP5_DEPLOY, "mp5" );
+#endif // defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+}
+
+#if defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+void CMP5::Holster(int skiplocal /*= 0*/)
+{
+	m_fInReload = FALSE;// cancel any reload in progress.
+
+	m_pPlayer->m_flNextAttack = UTIL_WeaponTimeBase() + 0.5;
+
+#ifndef CLIENT_DLL
+	if (m_pSpot)
+	{
+		m_pSpot->Killed(NULL, GIB_NEVER);
+		m_pSpot = NULL;
+	}
+#endif
+}
+#endif // defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+
+void CMP5::PrimaryAttack()
+{
+	// don't fire underwater
+	if (m_pPlayer->pev->waterlevel == 3)
+	{
+		PlayEmptySound( );
+		m_flNextPrimaryAttack = 0.15;
+		return;
+	}
+
+	if (m_iClip <= 0)
+	{
+		PlayEmptySound();
+		m_flNextPrimaryAttack = 0.15;
+		return;
+	}
+
+	m_pPlayer->m_iWeaponVolume = NORMAL_GUN_VOLUME;
+	m_pPlayer->m_iWeaponFlash = NORMAL_GUN_FLASH;
+
+	m_iClip--;
+
+
+	m_pPlayer->pev->effects = (int)(m_pPlayer->pev->effects) | EF_MUZZLEFLASH;
+
+	// player "shoot" animation
+	m_pPlayer->SetAnimation( PLAYER_ATTACK1 );
+
+	Vector vecSrc	 = m_pPlayer->GetGunPosition( );
+	Vector vecAiming = m_pPlayer->GetAutoaimVector( AUTOAIM_5DEGREES );
+	Vector vecDir;
+#if defined ( EFTD_DLL )
+	Vector vecSpread;
+
+	// Allow for higher accuracy when the player is crouching.
+	if ( m_pPlayer->pev->flags & FL_DUCKING )
+	{
+		vecSpread = VECTOR_CONE_1DEGREES;
+	}
+	else
+	{
+		vecSpread = VECTOR_CONE_3DEGREES;
+	}
+
+	// single player spread
+	vecDir = m_pPlayer->FireBulletsPlayer( 1, vecSrc, vecAiming, vecSpread, 8192, BULLET_PLAYER_MP5, 2, 0, m_pPlayer->pev, m_pPlayer->random_seed );
+#else
+#ifdef CLIENT_DLL
+	if ( !bIsMultiplayer() )
+#else
+	if ( !g_pGameRules->IsMultiplayer() )
+#endif
+	{
+		// optimized multiplayer. Widened to make it easier to hit a moving player
+		vecDir = m_pPlayer->FireBulletsPlayer( 1, vecSrc, vecAiming, VECTOR_CONE_6DEGREES, 8192, BULLET_PLAYER_MP5, 2, 0, m_pPlayer->pev, m_pPlayer->random_seed );
+	}
+	else
+	{
+		// single player spread
+		vecDir = m_pPlayer->FireBulletsPlayer( 1, vecSrc, vecAiming, VECTOR_CONE_3DEGREES, 8192, BULLET_PLAYER_MP5, 2, 0, m_pPlayer->pev, m_pPlayer->random_seed );
+	}
+#endif // defined ( EFTD_DLL )
+
+  int flags;
+#if defined( CLIENT_WEAPONS )
+	flags = FEV_NOTHOST;
+#else
+	flags = 0;
+#endif
+
+	PLAYBACK_EVENT_FULL( flags, m_pPlayer->edict(), m_usMP5, 0.0, (float *)&g_vecZero, (float *)&g_vecZero, vecDir.x, vecDir.y, 0, 0, 0, 0 );
+
+	if (!m_iClip && m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] <= 0)
+		// HEV suit - indicate out of ammo condition
+		m_pPlayer->SetSuitUpdate("!HEV_AMO0", FALSE, 0);
+
+	m_flNextPrimaryAttack = GetNextAttackDelay(0.1);
+
+	if ( m_flNextPrimaryAttack < UTIL_WeaponTimeBase() )
+		m_flNextPrimaryAttack = UTIL_WeaponTimeBase() + 0.1;
+
+	m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + UTIL_SharedRandomFloat( m_pPlayer->random_seed, 10, 15 );
+
+#if defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+	UpdateSpot();
+#endif // defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+}
+
+
+
+void CMP5::SecondaryAttack( void )
+{
+	// don't fire underwater
+	if (m_pPlayer->pev->waterlevel == 3)
+	{
+		PlayEmptySound( );
+		m_flNextPrimaryAttack = 0.15;
+		return;
+	}
+
+	if (m_pPlayer->m_rgAmmo[m_iSecondaryAmmoType] == 0)
+	{
+		PlayEmptySound( );
+		return;
+	}
+
+	m_pPlayer->m_iWeaponVolume = NORMAL_GUN_VOLUME;
+	m_pPlayer->m_iWeaponFlash = BRIGHT_GUN_FLASH;
+
+	m_pPlayer->m_iExtraSoundTypes = bits_SOUND_DANGER;
+	m_pPlayer->m_flStopExtraSoundTime = UTIL_WeaponTimeBase() + 0.2;
+			
+	m_pPlayer->m_rgAmmo[m_iSecondaryAmmoType]--;
+
+	// player "shoot" animation
+	m_pPlayer->SetAnimation( PLAYER_ATTACK1 );
+
+ 	UTIL_MakeVectors( m_pPlayer->pev->v_angle + m_pPlayer->pev->punchangle );
+
+	// we don't add in player velocity anymore.
+	CGrenade::ShootContact( m_pPlayer->pev, 
+							m_pPlayer->pev->origin + m_pPlayer->pev->view_ofs + gpGlobals->v_forward * 16, 
+							gpGlobals->v_forward * 800 );
+
+	int flags;
+#if defined( CLIENT_WEAPONS )
+	flags = FEV_NOTHOST;
+#else
+	flags = 0;
+#endif
+
+	PLAYBACK_EVENT( flags, m_pPlayer->edict(), m_usMP52 );
+	
+	m_flNextPrimaryAttack = GetNextAttackDelay(1);
+	m_flNextSecondaryAttack = UTIL_WeaponTimeBase() + 1;
+	m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + 5;// idle pretty soon after shooting.
+
+	if (!m_pPlayer->m_rgAmmo[m_iSecondaryAmmoType])
+		// HEV suit - indicate out of ammo condition
+		m_pPlayer->SetSuitUpdate("!HEV_AMO0", FALSE, 0);
+
+#if defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+#ifndef CLIENT_DLL
+	if (m_pSpot && m_fSpotActive)
+	{
+		m_pSpot->Suspend(1);
+	}
+#endif
+#endif // defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+}
+
+void CMP5::Reload( void )
+{
+	if ( m_pPlayer->ammo_9mm <= 0 )
+		return;
+
+#if defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+	int iResult = DefaultReload(MP5_MAX_CLIP, MP5_RELOAD, 3.0);
+
+	if (iResult)
+	{
+#ifndef CLIENT_DLL
+		if (m_pSpot && m_fSpotActive)
+		{
+			m_pSpot->Suspend(3.0);
+			m_flNextSecondaryAttack = UTIL_WeaponTimeBase() + 3.0;
+		}
+#endif
+	}
+#else
+	DefaultReload( MP5_MAX_CLIP, MP5_RELOAD, 1.5 );
+#endif // defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+}
+
+
+void CMP5::WeaponIdle( void )
+{
+#if defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+	UpdateSpot();
+#endif // defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+
+	ResetEmptySound( );
+
+	m_pPlayer->GetAutoaimVector( AUTOAIM_5DEGREES );
+
+	if ( m_flTimeWeaponIdle > UTIL_WeaponTimeBase() )
+		return;
+
+	int iAnim;
+	switch ( RANDOM_LONG( 0, 1 ) )
+	{
+	case 0:	
+		iAnim = MP5_LONGIDLE;	
+		break;
+	
+	default:
+	case 1:
+		iAnim = MP5_IDLE1;
+		break;
+	}
+
+	SendWeaponAnim( iAnim );
+
+	m_flTimeWeaponIdle = UTIL_SharedRandomFloat( m_pPlayer->random_seed, 10, 15 ); // how long till we do this again.
+}
+
+#if defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+void CMP5::UpdateSpot(void)
+{
+#ifndef CLIENT_DLL
+	if (m_fSpotActive)
+	{
+		if (!m_pSpot)
+		{
+			m_pSpot = CLaserSpot::CreateSpot();
+			m_pSpot->pev->scale = 0.5;
+		}
+
+		UTIL_MakeVectors(m_pPlayer->pev->v_angle);
+		Vector vecSrc = m_pPlayer->GetGunPosition();;
+		Vector vecAiming = gpGlobals->v_forward;
+
+		TraceResult tr;
+		UTIL_TraceLine(vecSrc, vecSrc + vecAiming * 8192, dont_ignore_monsters, ENT(m_pPlayer->pev), &tr);
+
+		UTIL_SetOrigin(m_pSpot->pev, tr.vecEndPos);
+	}
+#endif
+}
+#endif // defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+
+class CMP5AmmoClip : public CBasePlayerAmmo
+{
+	void Spawn( void )
+	{ 
+		Precache( );
+		SET_MODEL(ENT(pev), "models/w_9mmARclip.mdl");
+		CBasePlayerAmmo::Spawn( );
+	}
+	void Precache( void )
+	{
+		PRECACHE_MODEL ("models/w_9mmARclip.mdl");
+		PRECACHE_SOUND("items/9mmclip1.wav");
+	}
+	BOOL AddAmmo( CBaseEntity *pOther ) 
+	{ 
+		int bResult = (pOther->GiveAmmo( AMMO_MP5CLIP_GIVE, "9mm", _9MM_MAX_CARRY) != -1);
+		if (bResult)
+		{
+			EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/9mmclip1.wav", 1, ATTN_NORM);
+		}
+		return bResult;
+	}
+};
+LINK_ENTITY_TO_CLASS( ammo_mp5clip, CMP5AmmoClip );
+LINK_ENTITY_TO_CLASS( ammo_9mmAR, CMP5AmmoClip );
+
+
+
+class CMP5Chainammo : public CBasePlayerAmmo
+{
+	void Spawn( void )
+	{ 
+		Precache( );
+		SET_MODEL(ENT(pev), "models/w_chainammo.mdl");
+		CBasePlayerAmmo::Spawn( );
+	}
+	void Precache( void )
+	{
+		PRECACHE_MODEL ("models/w_chainammo.mdl");
+		PRECACHE_SOUND("items/9mmclip1.wav");
+	}
+	BOOL AddAmmo( CBaseEntity *pOther ) 
+	{ 
+		int bResult = (pOther->GiveAmmo( AMMO_CHAINBOX_GIVE, "9mm", _9MM_MAX_CARRY) != -1);
+		if (bResult)
+		{
+			EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/9mmclip1.wav", 1, ATTN_NORM);
+		}
+		return bResult;
+	}
+};
+LINK_ENTITY_TO_CLASS( ammo_9mmbox, CMP5Chainammo );
+
+
+class CMP5AmmoGrenade : public CBasePlayerAmmo
+{
+	void Spawn( void )
+	{ 
+		Precache( );
+		SET_MODEL(ENT(pev), "models/w_ARgrenade.mdl");
+		CBasePlayerAmmo::Spawn( );
+	}
+	void Precache( void )
+	{
+		PRECACHE_MODEL ("models/w_ARgrenade.mdl");
+		PRECACHE_SOUND("items/9mmclip1.wav");
+	}
+	BOOL AddAmmo( CBaseEntity *pOther ) 
+	{ 
+		int bResult = (pOther->GiveAmmo( AMMO_M203BOX_GIVE, "ARgrenades", M203_GRENADE_MAX_CARRY ) != -1);
+
+		if (bResult)
+		{
+			EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/9mmclip1.wav", 1, ATTN_NORM);
+		}
+		return bResult;
+	}
+};
+LINK_ENTITY_TO_CLASS( ammo_mp5grenades, CMP5AmmoGrenade );
+LINK_ENTITY_TO_CLASS( ammo_ARgrenades, CMP5AmmoGrenade );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
